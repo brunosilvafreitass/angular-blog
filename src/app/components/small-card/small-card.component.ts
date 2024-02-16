93f28c2c5b5afb156bc4ae9234822a04bbd5d6c3
brunosilvafreitass <brunodasilvafreitas@outlook.com>
2024-02-16 13:54:38 -0300

    feat: atualização do projeto

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -3,15 +3,22 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css'],
+  styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
-  @Input()
-  photoCover: string = '';
-  @Input()
-  cardTitle: string = '';
 
-  constructor() {}
+  @Input()
+  photoCover:string = ""
 
-  ngOnInit(): void {}
+  @Input()
+  cardTitle:string = ""
+
+  @Input()
+  Id:string="0"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
 }
